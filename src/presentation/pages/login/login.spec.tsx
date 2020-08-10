668a52d4cc11ff7e0b382b8fe9944c2e6844207c
Breno Sena <breno.fsena@gmail.com>
2020-08-10 18:31:49 -0300

    test: ensure SignUp calls SaveAccessToken on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -29,7 +29,7 @@ const makeSut = (params?: SutParams): SutTypes => {
       <Login
         validation={validationStub}
         authentication={authenticationSpy}
-        saveAccesstoken={saveAccessTokenMock}
+        saveAccessToken={saveAccessTokenMock}
       />
     </Router>
   )

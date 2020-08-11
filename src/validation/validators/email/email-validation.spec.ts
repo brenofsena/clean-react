ca9e89e7802b82853263dbc7868002d16a2520b3
Breno Sena <breno.fsena@gmail.com>
2020-08-11 10:47:28 -0300

    refactor: change Validation to receive an object instead of value

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,24 +2,27 @@ import { Emailvalidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
 import faker from 'faker'
 
-const makeSut = (): Emailvalidation => new Emailvalidation(faker.database.column())
+const makeSut = (field: string): Emailvalidation => new Emailvalidation(field)
 
 describe('EmailValidation', () => {
   test('Should return error if email is valid', () => {
-    const sut = makeSut()
-    const error = sut.validate(faker.random.word())
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ [field]: faker.random.word() })
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return error if email is valid', () => {
-    const sut = makeSut()
-    const error = sut.validate(faker.internet.email())
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ [field]: faker.internet.email() })
     expect(error).toBeFalsy()
   })
 
   test('Should return false if email is empty', () => {
-    const sut = makeSut()
-    const error = sut.validate('')
+    const field = faker.database.column()
+    const sut = makeSut(field)
+    const error = sut.validate({ [field]: '' })
     expect(error).toBeFalsy()
   })
 })
